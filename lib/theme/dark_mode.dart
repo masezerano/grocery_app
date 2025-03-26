9baa836039bc5026a1e056a2935f3cb65c8142b0
masezerano <masezeranotaylor@gmail.com>
2025-03-26 12:01:30 +0200

    first commit

---
+++ lib/theme/dark_mode.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.green.shade900,
+    primary: Colors.green.shade800,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900,
+    
+
+  ),
+);
